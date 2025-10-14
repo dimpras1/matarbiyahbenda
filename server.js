e69d71a57f6dba0ade79f8638d1c4cde26d92d1f
WafyPutraPangestu <wafy@raharja.info>
2025-10-14 15:38:40 +0700

    ekstrakulikuler

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Path helper (karena pakai ES module)
 const __filename = fileURLToPath(import.meta.url);
